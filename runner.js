c337bc8f22ecb5e68d17b7035adc3f6a7561fc6c
icarus <ellishogan95@gmail.com>
2020-09-12 02:42:02 -0400

    base finnished, now to fix functionality and then to react project

--- runner.js
+++ runner.js
@@ -24,19 +24,67 @@ const Runner = (maze) => {
         },
 
         findEndPoints: () => {
-
+            const check = (nodeVal) => {
+                let node = null;
+                let onv = openNode.map((i)=> i.value);
+                if (onv.includes(nodeVal)) {
+                    node = Node(nodeVal);
+                } else {
+                    openNodes.map((i) => {
+                        if (i.value == nodeVal) {
+                            node = i;
+                        }
+                    });
+                }
+                return node;
+            }
+            for (let x = 0; x < maze.layout.length; x++) {
+                for (let y = 0; y < maze.layout[x].length; y++) {
+                    let p = maze.layout[x][y];
+                    if (p == maze.startChar) {
+                        start = check([x, y]);
+                    } else if (p == maze.endChar) {
+                        end = check([x, y]);
+                    }
+                }
+            }
         },
 
         lookAround: (node) => {
             openNodes.map((i) => {
                 if (i.value[0] - 1 == node.value[0] && i.value[1] == node.value[1]) {
                     node.addChild(i);
-                } else if 
+                } else if (i.value[0] + 1 == node.value[0] && i.value[1] == node.value[1]) {
+                    node.addChild(i);
+                } else if (i.value[1] - 1 == node.value[1] && i.value[0] == node.value[0]) {
+                    node.addChild(i);
+                } else if (i.value[1] + 1 == node.value[1] && i.value[0] == node.value[0]) {
+                    node.addChild(i);
+                }
             });
         },
 
         makeNodePaths: () => {
-
+            toVisit.push(start);
+            while (toVisit.length > 0) {
+                [... toVisit].map((point) => {
+                    toVisit.remove(point);
+                    if !(visited.includes(point)) {
+                        lookAround(point);
+                        visited.add(point.value);
+                        let newPath = new Set(...point.path);
+                        newPath.add(point.value);
+                        point.children.map((i) => {
+                            i.setPath(newPath);
+                            if (i.value == end.value) {
+                                completed = true;
+                            } else {
+                                toVisit.push(i);
+                            }
+                        });
+                    }  
+                });
+            }
         },
 
         viewCompleted: () => {
